6791d8a982463bb7a9e1a6c284af6279c65daa2c
cyberdriver <1198201587@qq.com>
2020-04-13 16:46:50 +0800

    init

---
+++ mysite/nvr/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NvrConfig(AppConfig):
+    name = 'nvr'
---
+++ mysite/nvr/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register("users", views.UserViewSet)
+router.register("groups", views.GroupViewSet)
+
+
+urlpatterns = [
+    path('index', views.index),
+    path('', include(router.urls)),
+    path('api-auth/', include("rest_framework.urls")),
+]
